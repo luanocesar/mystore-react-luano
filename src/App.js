0eb865d07e337bca69b6c982d3e8f01ca9c5e0b7
Luano Barros <luanocesar@gmail.com>
2022-04-19 18:55:26 -0400

    make it better

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        I love you, Fernanda <3 <br/>
-        Testando Heroku App para deploy de um App React Js.. kkkk
+        I love you, Fernanda <br></br>
+        Testando Heroku App para deploy de um App React Js
       </header>
     </div>
   );
