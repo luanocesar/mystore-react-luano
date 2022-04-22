029e1c4204c9b9ac268326cdbb8987cd66d0dbfa
Luano Barros <luanocesar@gmail.com>
2022-04-21 21:39:00 -0400

    testing Github Action with Firebase 5

---
+++ build/precache-manifest.684ec4bf834954454a4134dff5e9025a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "26a65303cccb9f082a28",
+    "url": "/static/css/main.01549dfe.chunk.css"
+  },
+  {
+    "revision": "26a65303cccb9f082a28",
+    "url": "/static/js/main.26a65303.chunk.js"
+  },
+  {
+    "revision": "9863931913954e89bf2e",
+    "url": "/static/js/1.98639319.chunk.js"
+  },
+  {
+    "revision": "576b2c06e1bac60574c2",
+    "url": "/static/js/2.576b2c06.chunk.js"
+  },
+  {
+    "revision": "b29c9e157adc6ecf5862",
+    "url": "/static/js/runtime~main.b29c9e15.js"
+  },
+  {
+    "revision": "97356c7e94c2e86f58b656b3d5b53985",
+    "url": "/index.html"
+  }
+];
